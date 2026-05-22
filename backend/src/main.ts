ad70aae1d818a126f83bd0f92ad79ac06b01b6a8
AnthonyOrtiz8A <aortizo4@miumg.edu.gt>
2026-05-21 18:46:18 -0600

    Subir backend a render
    Segunda prueba

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,10 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-   app.enableCors({
-    origin: "http://localhost:5173", // dirección del frontend (Vite)
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, // si usas cookies o headers de autorización
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://gestion-de-rrhh-y-nomina.netlify.app',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
 
   await app.listen(process.env.PORT ?? 3000);
