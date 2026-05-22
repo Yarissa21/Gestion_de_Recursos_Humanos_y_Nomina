b80beefd3891078bb6df80f7a288ef43c604b0f9
AnthonyOrtiz8A <aortizo4@miumg.edu.gt>
2026-05-21 18:12:39 -0600

    Revert "Arreglo de backend"
    This reverts commit 472840f888955bce097eebd3ed57c84a800c6651.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  app.enableCors({
-    origin: process.env.FRONTEND_URL ?? "https://gestion-de-rrhh-y-nomina.netlify.app",
+   app.enableCors({
+    origin: "http://localhost:5173", // dirección del frontend (Vite)
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
+    credentials: true, // si usas cookies o headers de autorización
   });
 
   await app.listen(process.env.PORT ?? 3000);
